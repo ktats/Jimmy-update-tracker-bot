4c426b824d50e5ea2ff6a2040c3bf2ecd4fefaa1
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-08-28 22:07:59 -0700

    Finalized function assigned to jim's account

--- server/updateCheck.js
+++ server/updateCheck.js
@@ -7,20 +7,20 @@ const cheerio = require('cheerio');
 const { accountSid, authToken }= require('../config.js');
 const client = require('twilio')(accountSid, authToken);
 
-var sampleText = 'Go Be';
+var sampleText = 'Go Be67986';
 
 module.exports.updateCheck = (req, res) => {
-  const url = ('http://reddit.com/u/fizzthewiz/posts');
+  const url = ('http://reddit.com/user/jimmyjah/posts');
   rp(url)
    .then((html) => {
        const $ = cheerio.load(html);
        const posts = $('._eYtD2XCVieq6emjKBH3m').text();
-       if (posts.slice(10,15) === sampleText) {
-           console.log(posts.slice(10,15));
+       if (posts.slice(8,20) === sampleText) {
+           console.log(posts.slice(8,20));
            return true;
        } else {
            console.log('there was a change! expect a text soon');
-           sampleText = posts.slice(10,15);
+           sampleText = posts.slice(8,20);
         client.messages
         .create({
            body: `New post from Jim, here it is: ${posts.slice(10, 200)}`,
