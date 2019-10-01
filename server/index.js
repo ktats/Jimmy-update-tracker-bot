44adfd0952f0cb0ddf199c40a388f2853440e838
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-10-01 10:20:07 -0700

    Final working bot

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const { updateCheck } = require('./updateCheck.js');
 
 
 app.get('/test', (req, res) => {
-    res.send('Looks like the server is in fact running');
+    res.send('Looks like the server is in fact running! This time on Jim');
 });
 
 // setInterval(() => console.log(2+2), 1000);
 
 //twillio number (708) 498-3783
 
-setInterval(updateCheck, 10000);
+setInterval(updateCheck, 600000);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
